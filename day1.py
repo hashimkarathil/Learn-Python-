393f8dacf73f2ee384555e399e74cb753246901a
hashimkarathil <karathilhashim@gmail.com>
2026-06-03 09:34:23 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,132 @@
+# print("hello world")
+
+# if 5 > 2:
+#     print("5 is greater than 2")
+
+
+
+#....................................................
+# 1. Numeric Types 
+# age = 15
+# print(age)
+
+#....................................................
+
+# 1.1 float
+# float = 15.1
+# print(float)
+
+#....................................................
+
+# 2. str
+# name = "hashim"
+# print(name)
+
+#....................................................
+
+# 3. boolean
+# is_active = True
+# print(is_active)
+
+#....................................................
+
+# 4. List type
+
+# fruits = ["apple","banana","orange"]
+# print(fruits)
+# print(fruits[0])
+
+#....................................................
+
+# 5. Tuple type
+
+# fruits = ("apple","banana","orange","grapes")
+# print(fruits[0])
+
+#....................................................
+
+# 6. Dictionary Type
+
+# user = {"name":"Hashim","age":25}
+# print(user["name"])
+
+# 7 . Set Type
+# fruits = {"apple","orange","grape"}
+# print(fruits)
+
+
+
+
+# .................... Variable Assignment and Operations..............
+
+#Variable Assignment
+
+# x = 10
+# y = 3.5
+# name = "john"
+# is_student = True
+
+
+
+# 1. Arithematic operater
+# a=5
+# b=4
+# print(a+b)
+# print(a-b)
+# print(a%b)
+# print(a/b)
+
+# 2. Assignment opeartor
+# a=40
+# b=20
+
+# # print(a**b)
+# # print(a)
+# # print(a+5)
+# print(a==b)
+
+# 3. logical opeartor
+
+# a = 5
+# b = 10
+# print(a>b and b>a)
+# print(a>b or b>a)
+# print(not  b<a)
+
+# 4. comparison operator
+
+# a = 15
+# b = 10
+# print(a == b)
+# print(a != b)
+# print(a<b)
+
+
+
+
+# 5. identity operator
+#compare the memory location of two objects
+# a = 5
+# b = 5
+# print(a is not b)
+
+# 6. Membership operator
+#membership check for membership in sequence
+# a = "Hello world"
+# print("world" in a)
+
+
+
+#.......................Strings Overview ....................
+
+
+#........String Slicing
+
+#Basic Slicing:
+# s = "Hello, world"
+# print(s[0:5])
+
+#Negative Indices
+# print(s)
+
+
